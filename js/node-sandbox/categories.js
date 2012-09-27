f78440989f94520fbcbfdab06982731ddbfb1a5e
Louis Jeng <louis.jeng@teltel.com>
2012-09-27 15:07:21 +0800

    log error

--- js/node-sandbox/categories.js
+++ js/node-sandbox/categories.js
@@ -2,16 +2,18 @@
  * categories.js
  */
 
+//////// Compatible with front-end ////////
 if (typeof require == 'function') {
     if (typeof $ == 'undefined') {
         var $ = require('jquery');
     }
     eval(require('fs').readFileSync('../nn-sdk.js', 'utf8'));
 }
-
 if (typeof $ == 'undefined' || typeof nn == 'undefined') {
+    console.error('nothing loaded!');
     return;
 }
+///////////////////////////////////////////
 
 nn.api('GET', 'http://dev.teltel.com:8080/api/categories');
 
